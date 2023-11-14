cff9f3b138306b46d07882040872a7ee0f469ef0
Betelhem-Belete <betelhembelete0@gmail.com>
2023-11-13 23:00:52 -0500

    rrrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-import './App.css';
+import Register from './Register';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="bg-red-500">test</div>;
+  return <Register />;
 }
 
 export default App;
