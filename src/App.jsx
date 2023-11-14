42c55fd877eb1591ae846e473e52d0c1167799f5
Betelhem-Belete <betelhembelete0@gmail.com>
2023-11-13 22:33:12 -0500

    local to remote

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-500">test</div>;
+}
+
+export default App;
