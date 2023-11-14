3ef3ba6e3fef5f65beb5adb813f5b41f9445bf41
Betelhem-Belete <betelhembelete0@gmail.com>
2023-11-13 23:13:34 -0500

    rrrr

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,13 +4,17 @@ const Register = () => {
   return (
     <div className="bg-blue-50 h-screen">
       <form action="" className="w-64 mx-auto">
-        <input type="text" placeholder="username" className="block w-full" />
+        <input
+          type="text"
+          placeholder="username"
+          className="block w-full  rounded-sm p-2 mb-2 border"
+        />
         <input
           type="password"
           placeholder="password"
-          className="block w-full"
+          className="block w-full rounded-sm p-2 mb-2 border"
         />
-        <button className="bg-blue-500 text-white block w-full">
+        <button className="bg-blue-500 text-white block w-full rounded-sm">
           Register
         </button>
       </form>
