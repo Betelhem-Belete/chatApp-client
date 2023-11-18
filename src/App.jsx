690b72dc30b8e3cfcaa59e13ee7e6c105efcdbea
Betelhem-Belete <betelhembelete0@gmail.com>
2023-11-18 07:04:45 -0500

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Register from './Register';
+import axios from 'axios';
 
 function App() {
+  axios.defaults.baseURL = 'http://localhost:4000';
+  axios.defaults.withCredentials = true;
   return <Register />;
 }
 
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function Register(ev) {
+    ev.preventDefault();
+    await axios.post('/Register', { username, password });
+  }
   return (
-    <div className="bg-blue-50 h-screen">
-      <form action="" className="w-64 mx-auto">
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form action="" className="w-64 mx-auto" onSubmit={Register}>
         <input
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
           type="text"
           placeholder="username"
           className="block w-full  rounded-sm p-2 mb-2 border"
         />
         <input
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
           type="password"
           placeholder="password"
           className="block w-full rounded-sm p-2 mb-2 border"
